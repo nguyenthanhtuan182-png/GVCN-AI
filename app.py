6b8fcb93bc2a7700e9dbb1ce2774b23e7909144d
TomHumXanh <nguyenthanhtuan182@gmail.com>
2026-07-22 10:46:41 +0700

    Fix Render port

--- app.py
+++ app.py
@@ -772,10 +772,16 @@ def api_gender():
 # CHẠY CHƯƠNG TRÌNH
 # ==========================
 
+import os
+
 if __name__ == "__main__":
 
     init_database()
 
+    port = int(os.environ.get("PORT", 5000))
+
     app.run(
-        debug=True
+        host="0.0.0.0",
+        port=port,
+        debug=False
     )
